38adcbb84fba129bfacd2ff61c49b21cd74ee848
sakibhossain <sakibhossainceo@gmail.com>
2023-08-14 18:49:48 -0700

    level-03 html added

---
+++ Level-03/script.js
@@ -0,0 +1,8 @@
+/*
+practice task for local storage (level-03).
+step-1: Create a form where will have name,email,massege input and every input beside will have two button 1. Delete name 2. send name and after all in the bottom wil have Reset button.
+step-2: Take this inputs
+step-3: If you click on the 'Send Name' button, the input will be set on the local storage. Do the same for other "Send button".
+step-4:  If you click on the "Delete Name" button, the name key & value will be cleared from the local storage. Do the same for other "Delete buttons".
+step-5: If you click on the "Reset" button, the key & value will be cleared from local sotarage.
+*/
