ce5cbb02d5415739f1e3dbfb1c424f25a6297341
sakibhossain <sakibhossainceo@gmail.com>
2023-08-14 17:45:22 -0700

    level 02 done

--- Level-02/script.js
+++ Level-02/script.js
@@ -7,3 +7,34 @@ step-4: With every click on the button, the  result will be stored in the local 
 stap-5: Now the result is stored in local storage. So, if you reload the page or close the tab & open again, the result of "the count" is not 0. Rather it will show the value from local storage without reset.
 */
 
+
+const p = document.getElementById('number');
+const increaseBtn = document.getElementById('increaseBtn');
+
+let clickCount = 0;
+
+increaseBtn.addEventListener("click", () => {
+    clickCount++;
+    p.innerText = clickCount;
+    saveIncreaseNumber(clickCount);
+})
+
+const getStoredNumber = () => {
+    let number = {};
+    const increaseNumber = localStorage.getItem('number');
+    if(increaseNumber){
+        number = JSON.parse(increaseNumber);
+    }
+    return number;
+}
+
+const saveIncreaseNumber = (number) => {
+    localStorage.setItem('number', number);
+}
+
+const displayIncreaseNumberFromLocalStorage = () => {
+    const increaseNumber = getStoredNumber();
+    p.innerText = increaseNumber;
+}
+
+displayIncreaseNumberFromLocalStorage();
