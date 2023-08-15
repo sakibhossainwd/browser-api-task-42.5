5b4c7df5bb69c2ce84873bd6ef57a3ed50dad46d
sakibhossain <sakibhossainceo@gmail.com>
2023-08-14 19:13:02 -0700

    level-03 function added

--- Level-03/script.js
+++ Level-03/script.js
@@ -6,3 +6,41 @@ step-3: If you click on the 'Send Name' button, the input will be set on the loc
 step-4:  If you click on the "Delete Name" button, the name key & value will be cleared from the local storage. Do the same for other "Delete buttons".
 step-5: If you click on the "Reset" button, the key & value will be cleared from local sotarage.
 */
+
+
+// Name part
+const sendName = () => {
+    const name = document.getElementById('name').value;
+    localStorage.getItem('name');
+    localStorage.setItem('name', name)
+}
+const deleteName = () => {
+    localStorage.removeItem('name');
+}
+
+// Email part
+const sendEmail = () => {
+    const email = document.getElementById('email').value;
+    localStorage.getItem('email');
+    localStorage.setItem('email', email)
+}
+const deleteEmail = () => {
+    localStorage.removeItem('email');
+}
+
+// message part
+const sendMessage = () => {
+    const message = document.getElementById('message').value;
+    localStorage.getItem('message');
+    localStorage.setItem('message', message)
+}
+const deleteMessage = () => {
+    localStorage.removeItem('message');
+}
+
+// Reset part
+const reset = () => {
+    localStorage.removeItem('name');
+    localStorage.removeItem('email');
+    localStorage.removeItem('message');
+}
