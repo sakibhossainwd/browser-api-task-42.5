04538c85b42c4fb53f4cec34ecd1217c9d54faa5
sakibhossain <sakibhossainceo@gmail.com>
2023-09-02 21:31:33 -0700

    all are done

--- issue-tracker-master/main.js
+++ issue-tracker-master/main.js
@@ -31,7 +31,7 @@ const closeIssue = id => {
 
 const deleteIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
+  const remainingIssues = issues.filter( issues.id !== id )
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
 }
 
